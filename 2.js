fd330e4355c87c9c63264e8eedc0f6a25012aeb4
mladenovicd <dimitrije.mladenovic@outlook.com>
2020-07-27 12:14:23 +0200

    Resenja zadataka

--- 2.js
+++ 2.js
@@ -5,11 +5,17 @@ let red = '';
 
 for(let i = 1; i<n; i++){
     red = ' '.repeat(n-i) + '*'.repeat(i) + '*'.repeat(i-1) +' '.repeat(n-i);
-    console.log(red);
+    console.log(red); // gornji deo- uspravan trougao
 }
 
 red = '*'.repeat(n-1) + ' '.repeat(n-(n-1)) + '*'.repeat(n-1);
-console.log(red);
+console.log(red); // sredina
+
+for(let i = n-1; i<n;i--){
+    red = ' '.repeat(n-i) + '*'.repeat(i) + '*'.repeat(i-1) + ' '.repeat(n-1);
+    console.log(red); // donji deo- obrnuti trougao
+} 
+
 
 
 
